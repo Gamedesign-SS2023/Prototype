f5d5bcf699526d4f26e52af14ffdb211e2ff856a
q-academic <sandra.nguyen@alumni.fh-aachen.de>
2023-04-25 15:28:21 +0200

    adjust timer format

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
-    public float currentTime;
+    private float currentTime = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         currentTime += Time.deltaTime;
-        timerText.text = currentTime.ToString();
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        timerText.text = time.ToString(@"mm\:ss");
     }
 }
