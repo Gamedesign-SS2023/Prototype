182c28737eceaf9bb809d3d8ec08c765f95ce5c2
Sandaboy <94974403+Sandaboy@users.noreply.github.com>
2023-07-30 00:21:12 +0200

    Lullaby
    Soweit fertig ist zwar nicht ganz wie ich es mir vorgestellt habe aber ist jz auch egal wir haben keine Zeit mehr

---
+++ Assets/Scripts/Weapons/ProjectileLullaby.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ProjectileLullaby : MonoBehaviour
+{
+    [SerializeField] public float damage;
+    int hits;
+    public int weaponLVL;
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Enemy")
+        {
+            Enemy enemy = collider.gameObject.GetComponent<Enemy>();
+            
+            if (weaponLVL >= 3)
+            {
+                enemy.TakeDamage(damage * 2, 1);
+                if (!enemy.slowed)
+                {
+                    enemy.slowed = true;
+                    enemy.moveSpeed -= enemy.moveSpeed / 2;
+                    enemy.moveSpeed -= enemy.moveSpeed / 2;
+                }
+            }
+            else if(weaponLVL >= 1)
+            {
+                enemy.TakeDamage(damage * 2, 1);
+                if (!enemy.slowed)
+                {
+                    enemy.slowed = true;
+                    enemy.moveSpeed -= enemy.moveSpeed / 2;
+                }     
+            }
+            else
+            {
+                enemy.TakeDamage(damage, 1);
+                if (!enemy.slowed)
+                {
+                    enemy.slowed = true;
+                    enemy.moveSpeed -= enemy.moveSpeed / 3;
+                }   
+            }
+        }
+        if (collider.gameObject.tag == "Fence")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
